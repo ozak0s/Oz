411e7899bf6ad147d75cbabad43ba0f2faae18d8
ozak0s <83041482+ozak0s@users.noreply.github.com>
2021-04-23 00:57:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+"""
+This program assigns song atribute values
+to specific variables and then displays them to the user.
+"""
+SongName = "Hold Your Color" #assigns the value "Hold Your Color" (string) to the variable SongName
+Artist = "Pendulum" #assigns the value "Pendulum" (string) to the variable Artist
+VocalsBy = "Rob Swire" #assigns the value "Rob Swire" (string) to the variable VocalsBy
+LyricsBy = "Gareth McGrillen, Paul Harding" #assigns the value "Gareth McGrillen, Paul Harding" (string) to the variable LyricsBy
+MusicBy = "Rob Swire(Synthesiser,mixing), Andrew Goddard(guitar), Jon Stockman(bass)" #assings the value "Rob Swire(Synthesiser,mixing), Andrew Goddard(guitar), Jon Stockman(bass)" (string) to the variable MusicBy
+TrackListNumber = 10 #assings the value 10 (integer) to the variable TrackListNumber
+Genre = "Drum n Bass" #assings the value "Drum n Bass" (string) to the variable Genre
+SongDuration = float(5.42) #assings the value 5.42 (decimal/float) to the variable SongDuration
+SongReleaseDate = "6 March 2006" #assings the value "6 March 2006" (string) to the variable SongReleaseDate
+AlbumName = "Hold Your Color" #assigns the value "Hold Your Color" (string) to the variable AlbumName
+AlbumDuration = float(79.49) #assings the value 79.49 (decimal/float) to the variable AlbumDuration
+AlbumReleaseDate = "25 July 2005" #assigns the value "25 July 2005" (string) to the variable AlbumReleaseDate
+Label = "Breakbeat Kaos" #assigns the value "Breakbeat Kaos" (string) to the variable Label
+
+
+#Displays each variable value in order
+print(SongName)
+print(Artist)
+print(VocalsBy)
+print(LyricsBy)
+print(MusicBy)
+print(TrackListNumber)
+print(Genre)
+print(SongDuration)
+print(SongReleaseDate)
+print(AlbumName)
+print(AlbumDuration)
+print(AlbumReleaseDate)
+print(Label)
